36207e0673e69f0f9efd4b7618c0780128a946ea
lizhieffe <lizhieffe@gmail.com>
2017-10-29 08:03:50 -0700

    update greeting screen

--- init/entry.c
+++ init/entry.c
@@ -5,8 +5,10 @@ int kern_entry() {
   uint8_t *input = (uint8_t *)0xB8000;
   uint8_t color = (0 << 4) | (15 & 0x0F);
 
-  char greeting[] = "Hello, world!    - by lizhi";
-  console_write(greeting);
+  console_clear();
+
+  char greeting[] = "Hello, world!\n\t- by lizhi";
+  console_write_color(greeting, rc_black, rc_green);
 
   return 0;
 }
